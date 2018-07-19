a92de88db280692178ec74ee8c344d0b7dc0d312
tamkylet <kyle_tam1@hotmail.com>
2018-07-18 17:33:30 -0700

    node - express skeleton template

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+
+require('dotenv').load();
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('component')
+);
